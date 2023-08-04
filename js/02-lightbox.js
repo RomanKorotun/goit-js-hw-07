8af2235193344e9cf23493bca18b623ca383890a
RomanKorotun <127940013+RomanKorotun@users.noreply.github.com>
2023-08-04 21:25:32 +0300

    Update 02-lightbox.js

--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -17,7 +17,7 @@ function createMarkup(arr) {
   let markup = arr
     .map(
       ({ preview, original, description }) => `<li class="gallery__item">
-   <a class="gallery__link" href="${preview}">
+   <a class="gallery__link" href="${original}">
       <img class="gallery__image" src="${preview}" alt="${description}" />
    </a>
 </li>`
