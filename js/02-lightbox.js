585bb7261d7363d5820ad1fb5909a8b2bfdf0d6a
RomanKorotun <127940013+RomanKorotun@users.noreply.github.com>
2023-08-04 15:17:42 +0300

    Update 02-lightbox.js

--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -1,4 +1,41 @@
-import { galleryItems } from './gallery-items.js';
+import { galleryItems } from "./gallery-items.js";
 // Change code below this line
 
 console.log(galleryItems);
+
+// {
+//     preview:
+//       'https://cdn.pixabay.com/photo/2019/05/14/16/43/rchids-4202820__480.jpg',
+//     original:
+//       'https://cdn.pixabay.com/photo/2019/05/14/16/43/rchids-4202820_1280.jpg',
+//     description: 'Hokkaido Flower',
+//   },
+
+const galleryList = document.querySelector(".gallery");
+
+function createMarkup(arr) {
+  let markup = arr
+    .map(
+      ({ preview, original, description }) => `<li class="gallery__item">
+   <a class="gallery__link" href="${preview}">
+      <img class="gallery__image" src="${preview}" alt="${description}" />
+   </a>
+</li>`
+    )
+    .join("");
+  galleryList.insertAdjacentHTML("beforeend", markup);
+}
+createMarkup(galleryItems);
+
+galleryList.addEventListener("click", handlerClick);
+
+function handlerClick(evt) {
+  evt.preventDefault();
+  if (!evt.target.classList.contains("gallery__image")) {
+    return;
+  }
+  var lightbox = new SimpleLightbox(".gallery a", {
+    captionsData: "alt",
+    captionDelay: 250,
+  });
+}
